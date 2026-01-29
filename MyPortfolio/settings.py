9a44c6ae2b25590812271f9dddf3a07b7667ebd4
Aly <lmmoore@adt.com>
2026-01-29 06:13:05 -0800

    More Secret key debugging

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 if not SECRET_KEY:
     raise ValueError("No SECRET_KEY found in environment variables")
